08bba9e1fc3045260e5903292872943deb69eb8c
zuluked <63346452+zuluked@users.noreply.github.com>
2020-11-07 15:48:55 +0100

    Create Kondition.dart

---
+++ Kondition.dart
@@ -0,0 +1,13 @@
+main(){
+int zahlengrenze = 11;
+
+if(zahlengrenze > 10){
+  print("Die Zahlengrenze ist größer als 10"); 
+}else{
+  print("Die Zahlengrenze ist nicht größer als 10"); 
+}
+  
+String tatsache = zahlengrenze > 10 ? "Die Zahlengrenze ist größer als 10" :  "Die Zahlengrenze ist nicht größer als 10";
+  
+  print(tatsache);
+}
