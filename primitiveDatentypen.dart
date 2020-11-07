b6b2ffa56ee3ffa1a12ac40c145e489792bc3c68
zuluked <63346452+zuluked@users.noreply.github.com>
2020-11-07 14:18:02 +0100

    Create primitiveDatentypen.dart

---
+++ primitiveDatentypen.dart
@@ -0,0 +1,15 @@
+main() {
+  int zahl1 = 1;
+  int zahl2 = 2;
+  double kommazahl1 = 2.2;
+  double kommazahl2 = 2.34;
+  String vorname = "Zeyd";
+  String nachname = "Kederoglu";
+
+  print(zahl1);
+  print(zahl2);
+  print(kommazahl1);
+  print(kommazahl2);
+  print(vorname);
+  print(nachname);
+}
