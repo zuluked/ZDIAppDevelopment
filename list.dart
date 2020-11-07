affbbdfbd4508b34a64538237b0e7d60016374a8
zuluked <63346452+zuluked@users.noreply.github.com>
2020-11-07 16:18:23 +0100

    Create list.dart

---
+++ list.dart
@@ -0,0 +1,12 @@
+main(){
+List<String> schulfaecher = ["Mathe", "Deutsch", "Englisch", "Französisch"];
+
+
+for(int i = 0; i < schulfaecher.length; i++){
+  print(schulfaecher[i]);
+}
+  
+for(String fach in schulfaecher){
+  print(fach);
+}
+}
