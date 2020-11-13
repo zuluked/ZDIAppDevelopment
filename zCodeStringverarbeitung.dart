2e96f2dc9cab87eef201545386a642e9f8d6fd00
zuluked <63346452+zuluked@users.noreply.github.com>
2020-11-13 22:04:33 +0100

    Create zCodeStringverarbeitung.dart

---
+++ zCodeStringverarbeitung.dart
@@ -0,0 +1,9 @@
+main() {
+  String meinName = "Zeyd";
+  print("Mein Name ist $meinName!");
+  double groesse = 1.5;
+  String neueGroesse = groesse.toString();
+  print("Meine Größe beträgt " + neueGroesse + " Meter!");
+  int namenslaenge = meinName.length;
+  print("Außerdem ist mein Name $namenslaenge Buchstaben lang!");
+}
